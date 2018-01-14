a1d202d55ae3ed467ee38a82924a8e01f6d0fa11
Ruchir Mehta <ruchirmehta14@gmail.com>
2018-01-13 20:43:10 -0800

    Add files via upload

---
+++ Swift Projects/Postcard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Postcard 2
+//
+//  Created by Ruchir Mehta on 8/23/17.
+//  Copyright © 2017 Rucker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
