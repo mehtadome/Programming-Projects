950dce6573ef25f076943a7b81b66ebab271e548
Ruchir Mehta <ruchirmehta14@gmail.com>
2018-01-16 18:15:13 -0800

    Update Contents.swift

--- RobotC Automatic Trash Can.playground/Contents.swift
+++ RobotC Automatic Trash Can.playground/Contents.swift
@@ -9,8 +9,8 @@ var str = "Hello, playground"
 int deposits;
 task main(){
     deposits = 0;
-    while(1--1){
-        untilSonarLessThan(17, sonar);{   //when the sonar sensor detects and object:
+    while(1 == 1){
+        untilSonarLessThan(17, sonar);{   //when the sonar sensor detects an object:
             startMotor(rightMotor, 100) startMotor(leftMotor, -100);
             wait(1);                     	wait(1); //trash can lids open up for the trash and then close again after three seconds
             stopMotor(rightMotor);               	stopMotor(leftMotor);
@@ -18,11 +18,11 @@ task main(){
             wait(3);                     	wait(3);
             stopMotor(rightMotor);               	stopMotor(leftMotor);
             deposits = deposits+1;
-            if(deposit ==5){               	//If statement is to activate the trapdoor that lets out the garbage after five deposits
+            if(deposit == 5){               	//If statement is to activate the trapdoor that lets out the garbage after five deposits
                 startMotor(servoMotor, 2);
                 wait(5);
                 startMotor(servoMotor, 0);
-              	 startMotor(servoMtor, -100);
+              	startMotor(servoMtor, -100);
                 wait(5);
                 startMotor(servoMotor, 0);
                 deposits = 0;
