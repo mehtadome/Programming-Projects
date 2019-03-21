2af26a22a032f1f5d4beb58a5359fb1697e8d9bc
Ruchir Mehta <ruchirmehta14@gmail.com>
2019-03-21 13:24:02 -0700

    Create WeirdComplexFunction.java

---
+++ Java Projects/Visualization/WeirdComplexFunction.java
@@ -0,0 +1,31 @@
+package func;
+
+public class WeirdComplexFunction implements DoubleFunctionOfTwoInts
+{
+	@Override
+	public double fOfXY(int x, int y) 
+	{
+		Complex cOriginal = new Complex(x/125f, y/125f);
+		Complex c = new Complex(cOriginal);
+		
+		for (int n=1; n<=255; n++)
+		{
+			if (c.norm() >= 2)
+			{
+				if (n%2 == 0) 
+					//n = 0;
+					n *= 30;
+				return n;
+			}
+			c = Complex.multiply(c, c);
+			c = Complex.add(c, cOriginal);
+		}
+		return 0;
+	}
+	
+	@Override
+	public String getName()
+	{
+		return "Weird Complex Function";
+	}
+}
