d2e678bc98176b18661de846195f2e07635b7ec2
Ruchir Mehta <ruchirmehta14@gmail.com>
2019-03-21 13:05:00 -0700

    Create RecordFormatException.java

---
+++ Java Projects/DNA: Fasta & Fastq Converter/RecordFormatException.java
@@ -0,0 +1,13 @@
+package dna;
+
+public class RecordFormatException extends Exception
+{
+	
+	//constructor
+	public RecordFormatException(String message) {
+		
+		//passes the String to the superclass constructor 
+		super(message);
+		
+	}
+}
